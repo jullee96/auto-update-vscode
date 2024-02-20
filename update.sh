af11b223e2ded1b770721ecab622bc39950bf671
youngeun lee <youngeun.lee@k-futuretech.com>
2024-02-20 10:25:49 +0900

    update

--- update.sh
+++ update.sh
@@ -13,8 +13,15 @@ echo "CURRENT_VER : $CURRENT_VER"
 echo "INSTALLED_VER : $INSTALLED_VER"
 # CURRENT_VER=1.95
 if [[ $CURRENT_VER > $INSTALLED_VER ]]; then
-    echo "need to update" 
-    curl -L -o deb/code_$CURRENT_VER.deb 'https://code.visualstudio.com/sha/download?build=insider&os=cli-alpine-x64'
+    echo "*************************" 
+    echo "      need to update     " 
+    echo "*************************" 
+    # curl -L -o deb/code_$CURRENT_VER.deb 'https://code.visualstudio.com/docs/?dv=linux64_deb'
+    # curl -L -o deb/code_$CURRENT_VER.deb 'https://code.visualstudio.com/sha/download?build=insider&os=linux-deb-x64'
+    # https://code.visualstudio.com/sha/download?build=insider&os=linux-deb-arm64
+    # https://code.visualstudio.com/sha/download?build=insider&os=linux-deb-x64
+    wget 'https://code.visualstudio.com/sha/download?os=linux-deb-x64' -O deb/code_$CURRENT_VER.deb
+    
     sudo dpkg -i deb/code_$CURRENT_VER.deb
 else
     echo "same version" 
